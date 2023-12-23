5b390c6dd66c1e514893cab102600966cf442c70
Six <84689659+SixofClubsss@users.noreply.github.com>
2023-12-23 10:41:21 +0000

    tag 0.3.0

--- holdero/start.go
+++ holdero/start.go
@@ -27,7 +27,7 @@ import (
 
 const app_tag = "Holdero"
 
-var version = semver.MustParse("0.3.0-dev")
+var version = semver.MustParse("0.3.0")
 var gnomon = gnomes.NewGnomes()
 
 // Check holdero package version
