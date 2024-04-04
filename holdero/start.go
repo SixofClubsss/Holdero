67278484f82ff1c0eb0be2948de07a1b47b81460
Six <84689659+SixofClubsss@users.noreply.github.com>
2024-04-04 04:25:26 +0000

    mod to https://github.com/dReam-dApps/dReams/commit/83fb5ac73fd878c3aaea0728ebcfa153ad97ac6d

--- holdero/start.go
+++ holdero/start.go
@@ -27,7 +27,7 @@ const (
 	appID   = "dreamdapps.io.holdero"
 )
 
-var version = semver.MustParse("0.3.1-dev.3")
+var version = semver.MustParse("0.3.1-dev.4")
 var gnomon = gnomes.NewGnomes()
 
 // Check holdero package version
